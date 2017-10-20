91f8732ac502aafe784cde81f430f3a8b39e9c55
smit2300 <smithmick14@gmail.com>
2017-10-20 13:01:00 -0400

    resistorcalc

--- resistor_calc.m
+++
@@ -1,5 +0,0 @@
-function [Req_Parallel, Req_Series] = resistor_calc(Rs)
-Req_Parallel = 1/((1/(Rs(1))) + (1/(Rs(2))));
-Req_Series = Rs(1) + Rs(2);
-%fprintf('Parallel Equivalence: %.2f\nSeries Equivalence: %.2f\n', Req_Parallel, Req_Series);
-end
