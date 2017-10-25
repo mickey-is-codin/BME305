fa8fe60a3f19df2971f389f864f5ae67e2b9b90c
smit2300 <smithmick14@gmail.com>
2017-10-24 21:08:33 -0400

    Resonant Frequency Added

---
+++ MatlabFiles/ResonantFrequency.m
@@ -0,0 +1,18 @@
+clc;
+clear all;
+
+L = [6.8e-3, 3.3e-3, 15e-3, 15e-3];
+C = [68e-9, 68e-9, 33e-9, 15e-9];
+
+f0 = 1 ./ (2 .* pi .* sqrt(L .* C));
+
+fprintf('Resonant frequencies: ');
+
+for i = 1 : length(L)
+    fprintf('\n%.2f', f0(i));
+end
+
+fprintf('n');
+
+%Question 5 Code
+
